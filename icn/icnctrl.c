d558e1dbb339aa9ceef3f0be9e6588b1b06cc398
fritz <fritz>
1997-05-17 12:23:24 +0000

    Corrected some Copyright notes to refer to GPL.

--- icn/icnctrl.c
+++ icn/icnctrl.c
@@ -1,4 +1,4 @@
-/* $Id: icnctrl.c,v 1.1 1997/02/17 00:08:53 fritz Exp $
+/* $Id: icnctrl.c,v 1.2 1997/05/17 12:23:29 fritz Exp $
 
  * ICN-ISDN driver for Linux. (Control-Utility)
  *
@@ -7,20 +7,24 @@
  *
  * This file is part of Isdn4Linux.
  *
- * Isdn4Linux is distributed with NO WARRANTY OF ANY KIND.  No author
- * or distributor accepts any responsibility for the consequences of using it,
- * or for whether it serves any particular purpose or works at all, unless he
- * or she says so in writing.  Refer to the Isdn4Linux Free Public
- * License (the "License") for full details.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
- * Every copy of Isdn4Linux must include a copy of the License,
- * normally in a plain ASCII text file named LICENSE.  The License grants you
- * the right to copy, modify and redistribute Isdn4Linux, but only
- * under certain conditions described in the License.  Among other things, the
- * License requires that the copyright notice and this notice be preserved on
- * all copies.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * $Log: icnctrl.c,v $
+ * Revision 1.2  1997/05/17 12:23:29  fritz
+ * Corrected some Copyright notes to refer to GPL.
+ *
  * Revision 1.1  1997/02/17 00:08:53  fritz
  * New CVS tree
  *
--- imon/imon.c
+++ imon/imon.c
@@ -1,10 +1,27 @@
-/* $Id: imon.c,v 1.2 1997/03/20 00:19:01 luethje Exp $
+/* $Id: imon.c,v 1.3 1997/05/17 12:23:35 fritz Exp $
  *
  * iMON , extended version.
  * original iMON source (c) Michael Knigge
  * heavily modified and extended by Fritz Elfert
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  * $Log: imon.c,v $
+ * Revision 1.3  1997/05/17 12:23:35  fritz
+ * Corrected some Copyright notes to refer to GPL.
+ *
  * Revision 1.2  1997/03/20 00:19:01  luethje
  * inserted the line #include <errno.h> in avmb1/avmcapictrl.c and imon/imon.c,
  * some bugfixes, new structure in isdnlog/isdnrep/isdnrep.c.
