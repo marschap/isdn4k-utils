749b5a99abda92054e94912da19645275e723937
calle <calle>
1998-08-30 09:57:13 +0000

    I hope it is know readable for everybody.

---
+++ capi20/capi20.c
@@ -0,0 +1,326 @@
+/*
+ * $Id: capi20.c,v 1.3 1998/08/30 09:57:14 calle Exp $
+ * 
+ * $Log: capi20.c,v $
+ * Revision 1.3  1998/08/30 09:57:14  calle
+ * I hope it is know readable for everybody.
+ *
+ * Revision 1.1  1998/08/25 16:33:16  calle
+ * Added CAPI2.0 library. First Version.
+ *
+ */
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <linux/capi.h>
+#include "capi20.h"
+
+static int                  capi_fd = -1;
+static capi_ioctl_struct    ioctl_data;
+static unsigned char        rcvbuf[128+2048];   /* message + data */
+static unsigned char        sndbuf[128+2048];   /* message + data */
+
+unsigned capi20_isinstalled (void)
+{
+    if (capi_fd >= 0)
+        return 1;
+
+    /*----- open managment link -----*/
+    if ((capi_fd = open("/dev/capi20", O_RDWR, 0666)) < 0)
+        return 0;
+
+    return ioctl(capi_fd, CAPI_INSTALLED, 0) == 0;
+}
+
+/*
+ * managment of application ids
+ */
+
+static struct capi_applidmap {
+    int used;
+    int fd;
+} capi_applidmap[CAPI_MAXAPPL] = {0};
+
+static inline _cword allocapplid(int fd)
+{
+   _cword i;
+   for (i=0; i < CAPI_MAXAPPL; i++) {
+      if (capi_applidmap[i].used == 0) {
+         capi_applidmap[i].used = 1;
+         capi_applidmap[i].fd = fd;
+         return i+1;
+      }
+   }
+   return 0;
+}
+
+static inline void freeapplid(_cword applid)
+{
+    capi_applidmap[applid-1].used = 0;
+    capi_applidmap[applid-1].fd = -1;
+}
+
+static inline int validapplid(_cword applid)
+{
+    return applid > 0 && applid <= CAPI_MAXAPPL
+                      && capi_applidmap[applid-1].used;
+}
+
+static inline int applid2fd(_cword applid)
+{
+    if (applid < CAPI_MAXAPPL)
+	    return capi_applidmap[applid-1].fd;
+    return -1;
+}
+
+/* 
+ * CAPI2.0 functions
+ */
+
+_cword
+capi20_register (unsigned MaxB3Connection,
+		 unsigned MaxB3Blks,
+		 unsigned MaxSizeB3,
+		 CAPI_REGISTER_ERROR *ErrorCode)
+{
+    _cword applid;
+    char buf[PATH_MAX];
+    int i, fd = -1;
+
+    if (!capi20_isinstalled()) {
+       *ErrorCode = CapiRegNotInstalled;
+       return 0;
+    }
+
+    *ErrorCode = CapiRegOSResourceErr;
+
+    for (i=0; fd < 0; i++) {
+        /*----- open pseudo-clone device -----*/
+        sprintf(buf, "/dev/capi20.%02d", i);
+        if ((fd = open(buf, O_RDWR|O_NONBLOCK, 0666)) < 0) {
+            switch (errno) {
+            case EEXIST:
+                break;
+            default:
+                return 0;
+            }
+        }
+    }
+
+    if ((applid = allocapplid(fd)) == 0)
+        return 0;
+
+    ioctl_data.rparams.level3cnt = MaxB3Connection;
+    ioctl_data.rparams.datablkcnt = MaxB3Blks;
+    ioctl_data.rparams.datablklen = MaxSizeB3;
+
+    if (ioctl(fd, CAPI_REGISTER, &ioctl_data) < 0) {
+        if (errno == EIO) {
+            if (ioctl(fd, CAPI_GET_ERRCODE, &ioctl_data) < 0)
+                return 0;
+            *ErrorCode = ioctl_data.errcode;
+        }
+        return 0;
+    }
+    return applid;
+}
+
+MESSAGE_EXCHANGE_ERROR
+capi20_release (unsigned Appl_Id)
+{
+    if (!capi20_isinstalled())
+        return CapiRegNotInstalled;
+    if (!validapplid(Appl_Id))
+        return CapiIllAppNr;
+    (void)close(applid2fd(Appl_Id));
+    freeapplid(Appl_Id);
+    return CapiNoError;
+}
+
+MESSAGE_EXCHANGE_ERROR
+capi20_put_message (CAPI_MESSAGE Msg, unsigned Appl_Id)
+{
+    MESSAGE_EXCHANGE_ERROR ret;
+    int len = (Msg[0] | (Msg[1] << 8));
+    int cmd = Msg[4];
+    int subcmd = Msg[5];
+    int rc;
+    int fd;
+
+    if (!capi20_isinstalled())
+        return CapiRegNotInstalled;
+
+    if (!validapplid(Appl_Id))
+        return CapiIllAppNr;
+
+    fd = applid2fd(Appl_Id);
+
+    memcpy(sndbuf, Msg, len);
+
+    if (cmd == CAPI_DATA_B3 && subcmd == CAPI_REQ) {
+        int datalen = (Msg[16] | (Msg[17] << 8));
+        void *dataptr = (void *)(Msg[12]|(Msg[13]<<8)|(Msg[14]<<16)|(Msg[15]<<24));
+        memcpy(sndbuf+len, dataptr, datalen);
+        len += datalen;
+    }
+
+    ret = CapiNoError;
+    errno = 0;
+
+    if ((rc = write(fd, sndbuf, len)) != len) {
+        switch (errno) {
+            case EFAULT:
+            case EINVAL:
+                ret = CapiIllCmdOrSubcmdOrMsgToSmall;
+                break;
+            case EBADF:
+                ret = CapiIllAppNr;
+                break;
+            case EIO:
+                if (ioctl(fd, CAPI_GET_ERRCODE, &ioctl_data) < 0)
+                    ret = CapiMsgOSResourceErr;
+                else ret = (MESSAGE_EXCHANGE_ERROR)ioctl_data.errcode;
+                break;
+          default:
+                ret = CapiMsgOSResourceErr;
+                break;
+       }
+    }
+
+    return ret;
+}
+
+MESSAGE_EXCHANGE_ERROR
+capi20_get_message (unsigned Appl_Id, CAPI_MESSAGE  *ReturnMessage)
+{
+    MESSAGE_EXCHANGE_ERROR ret;
+    int rc, fd;
+
+    if (!capi20_isinstalled())
+        return CapiRegNotInstalled;
+
+    if (!validapplid(Appl_Id))
+        return CapiIllAppNr;
+
+    fd = applid2fd(Appl_Id);
+
+    *ReturnMessage = rcvbuf;
+    if ((rc = read(fd, rcvbuf, sizeof(rcvbuf))) > 0) {
+        return CapiNoError;
+    }
+
+    if (rc == 0)
+        return CapiReceiveQueueEmpty;
+
+    switch (errno) {
+        case EMSGSIZE:
+            ret = CapiIllCmdOrSubcmdOrMsgToSmall;
+            break;
+        case EAGAIN:
+            return CapiReceiveQueueEmpty;
+        default:
+            ret = CapiMsgOSResourceErr;
+            break;
+    }
+
+    return ret;
+}
+
+CAPI_MESSAGE
+capi20_get_manufacturer(unsigned contr, CAPI_MESSAGE LpBuffer)
+{
+    if (!capi20_isinstalled())
+       return 0;
+    ioctl_data.contr = contr;
+    if (ioctl(capi_fd, CAPI_GET_MANUFACTURER, &ioctl_data) < 0)
+       return 0;
+    strncpy(LpBuffer, ioctl_data.manufacturer, CAPI_MANUFACTURER_LEN);
+    return LpBuffer;
+}
+
+CAPI_MESSAGE
+capi20_get_version(unsigned contr, CAPI_MESSAGE version)
+{
+    if (!capi20_isinstalled())
+        return 0;
+    ioctl_data.contr = contr;
+    if (ioctl(capi_fd, CAPI_GET_VERSION, &ioctl_data) < 0)
+        return 0;
+    memcpy(version, &ioctl_data.version, sizeof(capi_version));
+    return version;
+}
+
+CAPI_MESSAGE 
+capi20_get_serial_number(unsigned contr, CAPI_MESSAGE LpBuffer)
+{
+    if (!capi20_isinstalled())
+        return 0;
+    ioctl_data.contr = contr;
+    if (ioctl(capi_fd, CAPI_GET_SERIAL, &ioctl_data) < 0)
+        return 0;
+    memcpy(LpBuffer, &ioctl_data.serial, CAPI_SERIAL_LEN);
+    return LpBuffer;
+}
+
+MESSAGE_EXCHANGE_ERROR
+capi20_get_profile(unsigned Controller, CAPI_MESSAGE LpBuffer)
+{
+    if (!capi20_isinstalled())
+        return CapiMsgNotInstalled;
+
+    ioctl_data.contr = Controller;
+    if (ioctl(capi_fd, CAPI_GET_PROFILE, &ioctl_data) < 0) {
+        if (errno != EIO)
+            return CapiMsgOSResourceErr;
+        if (ioctl(capi_fd, CAPI_GET_ERRCODE, &ioctl_data) < 0)
+            return CapiMsgOSResourceErr;
+        return (MESSAGE_EXCHANGE_ERROR)ioctl_data.errcode;
+    }
+    if (Controller)
+        memcpy(LpBuffer, &ioctl_data.profile, CAPI_SERIAL_LEN);
+    else
+        memcpy(LpBuffer, &ioctl_data.profile.ncontroller,
+                       sizeof(ioctl_data.profile.ncontroller));
+    return CapiNoError;
+}
+/*
+ * functions added to the CAPI2.0 spec
+ */
+
+MESSAGE_EXCHANGE_ERROR
+capi20_waitformessage(_cword Appl_Id, struct timeval *tvp)
+{
+  int fd;
+  fd_set rfds;
+  int retval;
+
+  FD_ZERO(&rfds);
+
+  if(!capi20_isinstalled())
+    return CapiRegNotInstalled;
+
+  if(!validapplid(Appl_Id))
+    return CapiIllAppNr;
+  
+  fd = applid2fd(Appl_Id);
+
+  FD_SET(fd, &rfds);
+  
+  fcntl(fd, F_SETFL, !O_NONBLOCK);
+  retval = select(fd + 1, &rfds, NULL, NULL, tvp);
+  fcntl(fd, F_SETFL, O_NONBLOCK);
+  
+  return(CapiNoError);
+}
+
+int
+capi20_fileno(_cword applid)
+{
+   return applid2fd(applid);
+}
+
---
+++ capi20/convert.c
@@ -0,0 +1,420 @@
+/*
+ * $Id: convert.c,v 1.3 1998/08/30 09:57:21 calle Exp $
+ *
+ * $Log: convert.c,v $
+ * Revision 1.3  1998/08/30 09:57:21  calle
+ * I hope it is know readable for everybody.
+ *
+ * Revision 1.1  1998/08/25 16:33:23  calle
+ * Added CAPI2.0 library. First Version.
+ *
+ */
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <time.h>
+#include <ctype.h>
+
+#include "capi20.h"
+
+typedef struct {
+    int typ;
+    unsigned off;
+} _cdef;
+
+#define _CBYTE	       1
+#define _CWORD	       2
+#define _CDWORD        3
+#define _CSTRUCT       4
+#define _CMSTRUCT      5
+#define _CEND	       6
+
+static _cdef cdef[] = {
+    /*00*/{_CEND},
+    /*01*/{_CEND},
+    /*02*/{_CEND},
+    /*03*/{_CDWORD,     offsetof(_cmsg,adr.adrController)	},
+    /*04*/{_CMSTRUCT,   offsetof(_cmsg,AdditionalInfo)	},
+    /*05*/{_CSTRUCT,    offsetof(_cmsg,B1configuration)	},
+    /*06*/{_CWORD,      offsetof(_cmsg,B1protocol)	},
+    /*07*/{_CSTRUCT,    offsetof(_cmsg,B2configuration)	},
+    /*08*/{_CWORD,      offsetof(_cmsg,B2protocol)	},
+    /*09*/{_CSTRUCT,    offsetof(_cmsg,B3configuration)	},
+    /*0a*/{_CWORD,      offsetof(_cmsg,B3protocol)	},
+    /*0b*/{_CSTRUCT,    offsetof(_cmsg,BC)	},
+    /*0c*/{_CSTRUCT,    offsetof(_cmsg,BChannelinformation)	},
+    /*0d*/{_CMSTRUCT,   offsetof(_cmsg,BProtocol)	},
+    /*0e*/{_CSTRUCT,    offsetof(_cmsg,CalledPartyNumber)	},
+    /*0f*/{_CSTRUCT,    offsetof(_cmsg,CalledPartySubaddress)	},
+    /*10*/{_CSTRUCT,    offsetof(_cmsg,CallingPartyNumber)	},
+    /*11*/{_CSTRUCT,    offsetof(_cmsg,CallingPartySubaddress)	},
+    /*12*/{_CDWORD,     offsetof(_cmsg,CIPmask)	},
+    /*13*/{_CDWORD,     offsetof(_cmsg,CIPmask2)	},
+    /*14*/{_CWORD,      offsetof(_cmsg,CIPValue)	},
+    /*15*/{_CDWORD,     offsetof(_cmsg,Class)	},
+    /*16*/{_CSTRUCT,    offsetof(_cmsg,ConnectedNumber)	},
+    /*17*/{_CSTRUCT,    offsetof(_cmsg,ConnectedSubaddress)	},
+    /*18*/{_CDWORD,     offsetof(_cmsg,Data)	},
+    /*19*/{_CWORD,      offsetof(_cmsg,DataHandle)	},
+    /*1a*/{_CWORD,      offsetof(_cmsg,DataLength)	},
+    /*1b*/{_CSTRUCT,    offsetof(_cmsg,FacilityConfirmationParameter)	},
+    /*1c*/{_CSTRUCT,    offsetof(_cmsg,Facilitydataarray)	},
+    /*1d*/{_CSTRUCT,    offsetof(_cmsg,FacilityIndicationParameter)	},
+    /*1e*/{_CSTRUCT,    offsetof(_cmsg,FacilityRequestParameter)	},
+    /*1f*/{_CSTRUCT,    offsetof(_cmsg,FacilityResponseParameters)	},
+    /*20*/{_CWORD,      offsetof(_cmsg,FacilitySelector)	},
+    /*21*/{_CWORD,      offsetof(_cmsg,Flags)	},
+    /*22*/{_CDWORD,     offsetof(_cmsg,Function)	},
+    /*23*/{_CSTRUCT,    offsetof(_cmsg,HLC)	},
+    /*24*/{_CWORD,      offsetof(_cmsg,Info)	},
+    /*25*/{_CSTRUCT,    offsetof(_cmsg,InfoElement)	},
+    /*26*/{_CDWORD,     offsetof(_cmsg,InfoMask)	},
+    /*27*/{_CWORD,      offsetof(_cmsg,InfoNumber)	},
+    /*28*/{_CSTRUCT,    offsetof(_cmsg,Keypadfacility)	},
+    /*29*/{_CSTRUCT,    offsetof(_cmsg,LLC)	},
+    /*2a*/{_CSTRUCT,    offsetof(_cmsg,ManuData)	},
+    /*2b*/{_CDWORD,     offsetof(_cmsg,ManuID)	},
+    /*2c*/{_CSTRUCT,    offsetof(_cmsg,NCPI)	},
+    /*2d*/{_CWORD,      offsetof(_cmsg,Reason)	},
+    /*2e*/{_CWORD,      offsetof(_cmsg,Reason_B3)	},
+    /*2f*/{_CWORD,      offsetof(_cmsg,Reject)	},
+    /*30*/{_CSTRUCT,    offsetof(_cmsg,Useruserdata)	},
+};
+
+static unsigned char *cpars[] = {
+    /*00*/ 0,
+    /*01 ALERT_REQ*/
+           (unsigned char*)"\x03\x04\x0c\x28\x30\x1c\x01\x01",
+    /*02 CONNECT_REQ*/
+          (unsigned char*)"\x03\x14\x0e\x10\x0f\x11\x0d\x06\x08\x0a\x05\x07\x09\x01\x0b\x29\x23\x04\x0c\x28\x30\x1c\x01\x01",
+    /*03*/ 0,
+    /*04 DISCONNECT_REQ*/
+          (unsigned char*)"\x03\x04\x0c\x28\x30\x1c\x01\x01",
+    /*05 LISTEN_REQ*/
+          (unsigned char*)"\x03\x26\x12\x13\x10\x11\x01",
+    /*06*/ 0,
+    /*07*/ 0,
+    /*08 INFO_REQ*/
+          (unsigned char*)"\x03\x0e\x04\x0c\x28\x30\x1c\x01\x01",
+    /*09 FACILITY_REQ*/
+          (unsigned char*)"\x03\x20\x1e\x01",
+    /*0a SELECT_B_PROTOCOL_REQ*/
+          (unsigned char*)"\x03\x0d\x06\x08\x0a\x05\x07\x09\x01\x01",
+    /*0b CONNECT_B3_REQ*/ 
+          (unsigned char*)"\x03\x2c\x01",
+    /*0c*/ 0,
+    /*0d DISCONNECT_B3_REQ*/
+          (unsigned char*)"\x03\x2c\x01",
+    /*0e*/ 0,
+    /*0f DATA_B3_REQ*/
+          (unsigned char*)"\x03\x18\x1a\x19\x21\x01",
+    /*10 RESET_B3_REQ*/
+          (unsigned char*)"\x03\x2c\x01",
+    /*11*/ 0,
+    /*12*/ 0,
+    /*13 ALERT_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*14 CONNECT_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*15*/ 0,
+    /*16 DISCONNECT_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*17 LISTEN_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*18 MANUFACTURER_REQ*/
+          (unsigned char*)"\x03\x2b\x15\x22\x2a\x01",
+    /*19*/ 0,
+    /*1a INFO_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*1b FACILITY_CONF*/
+          (unsigned char*)"\x03\x24\x20\x1b\x01",
+    /*1c SELECT_B_PROTOCOL_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*1d CONNECT_B3_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*1e*/ 0,
+    /*1f DISCONNECT_B3_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*20*/ 0,
+    /*21 DATA_B3_CONF*/
+          (unsigned char*)"\x03\x19\x24\x01",
+    /*22 RESET_B3_CONF*/
+          (unsigned char*)"\x03\x24\x01",
+    /*23*/ 0,
+    /*24*/ 0,
+    /*25*/ 0,
+    /*26 CONNECT_IND*/
+          (unsigned char*)"\x03\x14\x0e\x10\x0f\x11\x0b\x29\x23\x04\x0c\x28\x30\x1c\x01\x01",
+    /*27 CONNECT_ACTIVE_IND*/
+          (unsigned char*)"\x03\x16\x17\x29\x01",
+    /*28 DISCONNECT_IND*/
+          (unsigned char*)"\x03\x2d\x01",
+    /*29*/ 0,
+    /*2a MANUFACTURER_CONF*/ 
+          (unsigned char*)"\x03\x2b\x15\x22\x2a\x01",
+    /*2b*/ 0,
+    /*2c INFO_IND*/  
+          (unsigned char*)"\x03\x27\x25\x01",
+    /*2d FACILITY_IND*/ 
+          (unsigned char*)"\x03\x20\x1d\x01",
+    /*2e*/ 0,
+    /*2f CONNECT_B3_IND*/
+          (unsigned char*)"\x03\x2c\x01",
+    /*30 CONNECT_B3_ACTIVE_IND*/
+          (unsigned char*)"\x03\x2c\x01",
+    /*31 DISCONNECT_B3_IND*/
+          (unsigned char*)"\x03\x2e\x2c\x01",
+    /*32*/ 0,
+    /*33 DATA_B3_IND*/
+          (unsigned char*)"\x03\x18\x1a\x19\x21\x01",
+    /*34 RESET_B3_IND*/ 
+          (unsigned char*)"\x03\x2c\x01",
+    /*35 CONNECT_B3_T90_ACTIVE_IND*/
+          (unsigned char*)"\x03\x2c\x01",
+    /*36*/ 0,
+    /*37*/ 0,
+    /*38 CONNECT_RESP*/
+          (unsigned char*)"\x03\x2f\x0d\x06\x08\x0a\x05\x07\x09\x01\x16\x17\x29\x04\x0c\x28\x30\x1c\x01\x01",
+    /*39 CONNECT_ACTIVE_RESP*/
+          (unsigned char*)"\x03\x01",
+    /*3a DISCONNECT_RESP*/
+          (unsigned char*)"\x03\x01",
+    /*3b*/ 0,
+    /*3c MANUFACTURER_IND*/
+          (unsigned char*)"\x03\x2b\x15\x22\x2a\x01",
+    /*3d*/ 0,
+    /*3e INFO_RESP*/ 
+          (unsigned char*)"\x03\x01",
+    /*3f FACILITY_RESP*/
+          (unsigned char*)"\x03\x20\x1f\x01",
+    /*40*/ 0,
+    /*41 CONNECT_B3_RESP*/
+          (unsigned char*)"\x03\x2f\x2c\x01",
+    /*42 CONNECT_B3_ACTIVE_RESP*/
+          (unsigned char*)"\x03\x01",
+    /*43 DISCONNECT_B3_RESP*/
+          (unsigned char*)"\x03\x01",
+    /*44*/ 0,
+    /*45 DATA_B3_RESP*/
+          (unsigned char*)"\x03\x19\x01",
+    /*46 RESET_B3_RESP*/
+          (unsigned char*)"\x03\x01",
+    /*47 CONNECT_B3_T90_ACTIVE_RESP*/
+          (unsigned char*)"\x03\x01",
+    /*48*/ 0,
+    /*49*/ 0,
+    /*4a*/ 0,
+    /*4b*/ 0,
+    /*4c*/ 0,
+    /*4d*/ 0,
+    /*4e MANUFACTURER_RESP*/
+          (unsigned char*)"\x03\x2b\x15\x22\x2a\x01",
+};
+
+/*-------------------------------------------------------*/
+
+#define byteTLcpy(x,y)        *(_cbyte *)(x)=*(_cbyte *)(y);
+#define wordTLcpy(x,y)        *(_cword *)(x)=*(_cword *)(y);
+#define dwordTLcpy(x,y)       memcpy(x,y,4);
+#define structTLcpy(x,y,l)    memcpy (x,y,l)
+#define structTLcpyovl(x,y,l) memmove (x,y,l)
+
+#define byteTRcpy(x,y)        *(_cbyte *)(y)=*(_cbyte *)(x);
+#define wordTRcpy(x,y)        *(_cword *)(y)=*(_cword *)(x);
+#define dwordTRcpy(x,y)       memcpy(y,x,4);
+#define structTRcpy(x,y,l)    memcpy (y,x,l)
+#define structTRcpyovl(x,y,l) memmove (y,x,l)
+
+/*-------------------------------------------------------*/
+static unsigned command_2_index (unsigned c, unsigned sc) {
+    if (c & 0x80) c = 0x9+(c&0x0f);
+    else if (c<=0x0f) ;
+    else if (c==0x41) c = 0x9+0x1;
+    else if (c==0xff) c = 0x00;
+    return (sc&3)*(0x9+0x9)+c;
+}
+
+/*-------------------------------------------------------*/
+#define TYP (cdef[cmsg->par[cmsg->p]].typ)
+#define OFF (((char *)cmsg)+cdef[cmsg->par[cmsg->p]].off)
+
+static void jumpcstruct (_cmsg *cmsg) {
+    unsigned layer;
+    for (cmsg->p++,layer=1; layer;) {
+	assert (cmsg->p);
+	cmsg->p++;
+	switch (TYP) {
+	    case _CMSTRUCT:
+		layer++;
+		break;
+	    case _CEND:
+		layer--;
+		break;
+	}
+    }
+}
+/*-------------------------------------------------------*/
+static void PARS_2_MESSAGE (_cmsg *cmsg) {
+
+    for (;TYP != _CEND; cmsg->p++) {
+	switch (TYP) {
+	    case _CBYTE:
+		byteTLcpy (cmsg->m+cmsg->l, OFF);
+		cmsg->l++;
+		break;
+	    case _CWORD:
+		wordTLcpy (cmsg->m+cmsg->l, OFF);
+		cmsg->l+=2;
+		break;
+	    case _CDWORD:
+		dwordTLcpy (cmsg->m+cmsg->l, OFF);
+		cmsg->l+=4;
+		break;
+	    case _CSTRUCT:
+		if (*(CAPI_MESSAGE *) OFF == 0) {
+		    *(cmsg->m+cmsg->l)='\0';
+		    cmsg->l++;
+		}
+		else if(**(_cstruct*)OFF != 0xff) {
+		    structTLcpy (cmsg->m+cmsg->l, *(_cstruct*)OFF, 1+**(_cstruct*)OFF);
+		    cmsg->l+=1+**(_cstruct*)OFF;
+		}
+		else {
+		    _cstruct s = *(_cstruct*)OFF;
+		    structTLcpy (cmsg->m+cmsg->l, s, 3+*(_cword*)(s+1));
+		    cmsg->l+= 3+*(_cword*)(s+1);
+		}
+		break;
+	    case _CMSTRUCT:
+		/*----- Metastruktur 0 -----*/
+		if (*(_cmstruct*)OFF == CAPI_DEFAULT) {
+		    *(cmsg->m+cmsg->l)='\0';
+		    cmsg->l++;
+		    jumpcstruct (cmsg);
+		}
+		/*----- Metastruktur wird composed -----*/
+		else {
+		    unsigned _l = cmsg->l;
+		    unsigned _ls;
+		    cmsg->l++;
+		    cmsg->p++;
+		    PARS_2_MESSAGE (cmsg);
+		    _ls = cmsg->l-_l-1;
+		    if (_ls < 255)
+			(cmsg->m+_l)[0] = (_cbyte)_ls;
+		    else {
+			structTLcpyovl (cmsg->m+_l+3, cmsg->m+_l+1, _ls);
+			(cmsg->m+_l)[0] = 0xff;
+			wordTLcpy (cmsg->m+_l+1, &_ls);
+                        cmsg->l+=2;
+		    }
+		}
+		break;
+	}
+    }
+}
+
+/*-------------------------------------------------------*/
+unsigned capi_cmsg2message (_cmsg *cmsg, CAPI_MESSAGE msg)
+{
+    cmsg->m = msg;
+    cmsg->l = 8;
+    cmsg->p = 0;
+    cmsg->par = cpars [command_2_index (cmsg->Command,cmsg->Subcommand)];
+
+    PARS_2_MESSAGE (cmsg);
+
+    wordTLcpy (msg+0, &cmsg->l);
+    byteTLcpy (cmsg->m+4, &cmsg->Command);
+    byteTLcpy (cmsg->m+5, &cmsg->Subcommand);
+    wordTLcpy (cmsg->m+2, &cmsg->ApplId);
+    wordTLcpy (cmsg->m+6, &cmsg->Messagenumber);
+
+    return 0;
+}
+
+/*-------------------------------------------------------*/
+static void MESSAGE_2_PARS (_cmsg *cmsg) {
+    for (;TYP != _CEND; cmsg->p++) {
+
+	switch (TYP) {
+	    case _CBYTE:
+		byteTRcpy (cmsg->m+cmsg->l, OFF);
+		cmsg->l++;
+		break;
+	    case _CWORD:
+		wordTRcpy (cmsg->m+cmsg->l, OFF);
+		cmsg->l+=2;
+		break;
+	    case _CDWORD:
+		dwordTRcpy (cmsg->m+cmsg->l, OFF);
+		cmsg->l+=4;
+		break;
+	    case _CSTRUCT:
+		*(CAPI_MESSAGE *)OFF = cmsg->m+cmsg->l;
+
+		if (cmsg->m[cmsg->l] != 0xff)
+		    cmsg->l+= 1+ cmsg->m[cmsg->l];
+		else
+		    cmsg->l+= 3+ *(_cword *)(cmsg->m+cmsg->l+1);
+		break;
+	    case _CMSTRUCT:
+		/*----- Metastruktur 0 -----*/
+		if (cmsg->m[cmsg->l] == '\0') {
+		    *(_cmstruct*)OFF = CAPI_DEFAULT;
+		    cmsg->l++;
+		    jumpcstruct (cmsg);
+		}
+		else {
+		    unsigned _l = cmsg->l;
+		    *(_cmstruct*)OFF = CAPI_COMPOSE;
+		    cmsg->l = (cmsg->m+_l)[0] == 255 ? cmsg->l+3 : cmsg->l+1;
+		    cmsg->p++;
+		    MESSAGE_2_PARS (cmsg);
+		}
+		break;
+	}
+    }
+}
+
+/*-------------------------------------------------------*/
+unsigned capi_message2cmsg (_cmsg *cmsg, CAPI_MESSAGE msg)
+{
+    memset (cmsg, 0, sizeof(_cmsg));
+    cmsg->m = msg;
+    cmsg->l = 8;
+    cmsg->p = 0;
+    byteTRcpy (cmsg->m+4, &cmsg->Command);
+    byteTRcpy (cmsg->m+5, &cmsg->Subcommand);
+    cmsg->par = cpars [command_2_index (cmsg->Command,cmsg->Subcommand)];
+
+    MESSAGE_2_PARS (cmsg);
+
+    wordTRcpy (msg+0, &cmsg->l);
+    wordTRcpy (cmsg->m+2, &cmsg->ApplId);
+    wordTRcpy (cmsg->m+6, &cmsg->Messagenumber);
+
+    return 0;
+}
+
+/*-------------------------------------------------------*/
+unsigned capi_cmsg_answer (_cmsg *cmsg)
+{
+    cmsg->Subcommand |= 0x01;
+    return 0;
+}
+
+/*-------------------------------------------------------*/
+unsigned capi_cmsg_header (_cmsg *cmsg, _cword _ApplId,
+				 _cbyte _Command, _cbyte _Subcommand,
+				 _cword _Messagenumber, _cdword _Controller) {
+    memset (cmsg, 0, sizeof(_cmsg));
+    cmsg->ApplId = _ApplId ;
+    cmsg->Command = _Command ;
+    cmsg->Subcommand = _Subcommand ;
+    cmsg->Messagenumber = _Messagenumber;
+    cmsg->adr.adrController = _Controller ;
+    return 0;
+}
