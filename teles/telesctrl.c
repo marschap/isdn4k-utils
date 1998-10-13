835bb26785fbb325568dfbb9d1b75b0d8f20933e
keil <keil>
1998-10-13 23:07:31 +0000

    Added ISAR firmware support

--- teles/telesctrl.c
+++ teles/telesctrl.c
@@ -8,6 +8,33 @@
 #include <linux/isdn.h>
 
 char           *progname;
+unsigned char         *buffer = NULL;
+#define MAX_SIZE 0x10000
+
+unsigned char *
+read_firmware(unsigned char *fname)
+{
+	FILE *infile;
+	int  *p, cnt;
+	
+	if (!(infile = fopen(fname, "rb"))) {
+		fprintf(stderr, "cannot open file %s\n", fname);
+		exit(-1);
+	}
+	p = (int *) buffer = (unsigned char *) malloc(MAX_SIZE+4);
+	if (!buffer) {
+		fprintf(stderr, "cannot get %d byte memory\n", MAX_SIZE+4);
+		exit(-1);
+	}
+	cnt = fread(buffer + 4, 1, MAX_SIZE, infile);
+	fclose(infile);
+	if (cnt==MAX_SIZE) {
+		fprintf(stderr, "wrong filesize\n");
+		exit(-1);
+	}
+	*p = cnt;
+	return(buffer);
+}
 
 void 
 usage()
@@ -32,8 +59,12 @@ main(int argc, char *argv[])
 		if (argc != 4)
 			usage();
 		strcpy(ioctl_s.drvid, argv[1]);
-		ioctl_s.arg = strtol(argv[3], NULL, 0);
 		cmd = strtol(argv[2], NULL, 0);
+		if (cmd == 9) {
+			ioctl_s.arg = (ulong) read_firmware(argv[3]);
+		} else {
+			ioctl_s.arg = strtol(argv[3], NULL, 0);
+		}
 	} else {
 		ioctl_s.drvid[0] = '\0';
 		ioctl_s.arg = 0;
@@ -47,5 +78,7 @@ main(int argc, char *argv[])
 	if (ioctl(fd, IIOCDRVCTL + cmd, &ioctl_s) < 0)
 		perror(argv[1]);
 	close(fd);
+	if (buffer)
+		free(buffer);
 	return 0;
 }
