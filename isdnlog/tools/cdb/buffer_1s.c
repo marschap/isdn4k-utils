61101037dc84f38aeb2974ce13ce6c6d80045204
akool <akool>
2000-08-14 18:32:49 +0000

    non-GPL "cdb" totally removed

--- isdnlog/tools/cdb/buffer_1s.c
+++
@@ -1,6 +0,0 @@
-#include "readwrite.h"
-#include "buffer.h"
-
-char buffer_1small_space[256];
-static cdb_buffer it = BUFFER_INIT(write,1,buffer_1small_space,sizeof buffer_1small_space);
-cdb_buffer *buffer_1small = &it;
--- isdnlog/tools/cdb/buffer_2.c
+++
@@ -1,6 +0,0 @@
-#include "readwrite.h"
-#include "buffer.h"
-
-char buffer_2_space[256];
-static cdb_buffer it = BUFFER_INIT(write,2,buffer_2_space,sizeof buffer_2_space);
-cdb_buffer *buffer_2 = &it;
